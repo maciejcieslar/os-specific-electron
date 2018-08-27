b69a92c43215cc3b0d539ca43f3574c6237f37d5
maciejcieslar <marekmckl@gmail.com>
2018-08-27 21:45:26 +0200

    adding examples

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 const {
   byOS,
   platforms: { WINDOWS, MAC, LINUX },
+  byRelease,
+  releases: { WIN7, WIN8, WIN10 },
 } = require('./os');
 
-const getHomePath = byOS({
-  [MAC]: (username) => {
-    return `/Users/${username}`;
-  },
-  [WINDOWS]: (username) => {
-    return `\\Users\\${username}`;
-  },
-  [LINUX]: () => {
-    return `/home/${username}`;
-  },
-  default: (username) => {
-    return `/Users/${username}`;
-  },
+const whatIsHeUsing = byOS({
+  [MAC]: username => `Hi ${username}! You are using Mac.`,
+  [WINDOWS]: username => `Hi ${username}! You are using Windows.`,
+  [LINUX]: username => `Hi ${username}! You are using Linux.`,
+  default: username => `Hi ${username}! You are using something different.`,
 });
 
-const homePath = getHomePath('maciejcieslar');
+console.log(whatIsHeUsing('Maciej Cieslar')); // => Hi Maciej Cieslar! You are using Mac.
 
-console.log(homePath); // -> /Users/maciejcieslar
+const whatWindowsIsHeUsing = byOS({
+  [WINDOWS]: byRelease({
+    [WIN7]: username => `Hi ${username}! You are using Windows 7.`,
+    [WIN8]: username => `Hi ${username}! You are using Windows 8.`,
+    [WIN10]: username => `Hi ${username}! You are using Windows 10.`,
+  }),
+});
+
+console.log(whatWindowsIsHeUsing('Maciej Cieslar')); // => Hi Maciej Cieslar! You are using Windows 7.
